171bd861e197aa1759b40a4826c2440cd628f735
modesty0310 <rlatprua4@gmail.com>
2021-01-12 11:58:50 +0900

    dataset을 활용해서 click이벤트 처리

--- main.js
+++ main.js
@@ -1,28 +1,73 @@
 const result = document.querySelector('.result');
 const input = document.querySelector('.input');
 const items = document.querySelector('.items');
+const buttons = document.querySelectorAll('button')
 let numberCheck = false;
 
 
-items.addEventListener('click', e => {
+buttons.forEach(btn => {
+    btn.addEventListener('click', e => {
+        if(e.target.className === 'number'){
+            updateDisplay(e);
+            numberCheck = true;
+            return;
+        }
+        switch(btn.dataset.type) {
+            case 'operator':
+                if(numberCheck){
+                    updateDisplay(e);
+                    numberCheck = false;
+                }
+                input.focus();
+                break;
+            case 'CE' :
+                result.innerText = "";
+                input.value = "";
+                input.focus();
+                numberCheck = true;
+                break;
+            case 'C' :
+                input.value = "";
+                input.focus();
+                numberCheck = true;
+                break;
+            case 'equal' :
+                numberCheck = true;
+                calculate();
+                break;
+            case 'back-space' :
+                input.value = input.value.substring(0,input.value.length-1);
+                input.focus();
+                break;
+        }
+    })
+})
+
+function updateDisplay (e) {
     const text = e.target.innerText;
-    // let text = input.innerText;
-    if(text === "="){
-        calculate();
-        return;
-    }else if(text === "C"){
-        input.value = "";
-        input.focus();
-        return;
-    }else if(text === "CE"){
-        result.innerText = "";
-        input.value = "";
-        input.focus();
-        return;
-    }
     input.value += text;
     input.focus();
-});
+}
+
+// items.addEventListener('click', e => {
+//     const text = e.target.innerText;
+//     // let text = input.innerText;
+//     if(text === "="){
+//         calculate();
+//         return;
+//     }else if(text === "C"){
+//         input.value = "";
+//         input.focus();
+//         return;
+//     }else if(text === "CE"){
+//         result.innerText = "";
+//         input.value = "";
+//         input.focus();
+//         return;
+//     }
+//     input.value += text;
+//     input.focus();
+// });
 
 input.addEventListener('keypress', e=>{
     if(e.key === 'Enter'){
@@ -57,7 +102,6 @@ function numCheck(event) {
 
 function calculate () {
     if(result.innerText && (input.value[0] === '+' || input.value[0] === '-' || input.value[0] === '*' || input.value[0] === '/')){
-        console.log(result.innerText);
         result.innerText += input.value;
         result.innerText = eval(result.innerText);
         input.value = "";
