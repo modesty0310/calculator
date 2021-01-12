01d9b343d3e2e9b4d759be473af742d817133748
modesty0310 <rlatprua4@gmail.com>
2021-01-12 11:19:57 +0900

    결과값에 값이 있다면 연결해서 계산, 연산부호 연속 사용 막음

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const result = document.querySelector('.result');
 const input = document.querySelector('.input');
 const items = document.querySelector('.items');
+let numberCheck = false;
 
 
 items.addEventListener('click', e => {
     const text = e.target.innerText;
-    console.log(text);
     // let text = input.innerText;
     if(text === "="){
         calculate();
@@ -20,7 +20,7 @@ items.addEventListener('click', e => {
         input.focus();
         return;
     }
-    input.value = input.value+text;
+    input.value += text;
     input.focus();
 });
 
@@ -29,7 +29,10 @@ input.addEventListener('keypress', e=>{
         calculate();
         return;
     }
+
 })
+
+
 
 
 function numCheck(event) { 
@@ -37,10 +40,15 @@ function numCheck(event) {
     || event.key === '-'
     || event.key === '+'
     || event.key === '*'
-    || event.key === '/'
-    || event.key === 'Eneter'
+    || event.key === '/')
+        {if(numberCheck){
+            numberCheck = false;
+            return true
+        }
+    }
+    else if(event.key === 'Eneter'
     || event.key >= 0 && event.key <= 9) {
-
+        numberCheck = true;
     return true;
     }
     return false;
@@ -48,6 +56,13 @@ function numCheck(event) {
 
 
 function calculate () {
+    if(result.innerText && (input.value[0] === '+' || input.value[0] === '-' || input.value[0] === '*' || input.value[0] === '/')){
+        console.log(result.innerText);
+        result.innerText += input.value;
+        result.innerText = eval(result.innerText);
+        input.value = "";
+        return;
+    }
     result.innerText = eval(input.value);
     input.value = "";
     return;
